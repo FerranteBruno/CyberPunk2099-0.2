436c4050fd1a380644e6fd21536dd2412139a787
Ferrante, Bruno <bruno.ferrante@alumnos.frgp.utn.edu.ar>
2020-11-12 21:23:46 -0300

    asd123

--- TileEngine.cpp
+++ TileEngine.cpp
@@ -1,24 +1,23 @@
 #include "TileEngine.h"
 #include <iostream>
 
-float **tileEngine::loadMap()
+float** tileEngine::loadMap()
 {
 
 	FILE* p;
-
 
 	fopen_s(&p,"IMG/capoide.csv", "r");
 
 	map = (float**)malloc((filas) * sizeof(float*));
 	for (int i = 0; i < filas; i++) {
-		map[i] = (float*)malloc((columnas) * sizeof(float*));
+		map[i] = (float*)malloc((columnas) * sizeof(float));
 	}
 
-	for (int i = 0; i < filas; i++) {
+	for (int i = 0; i < columnas; i++) {
 
-		for (int j = 0; j < columnas; j++) {
+		for (int j = 0; j < filas; j++) {
 
-			fscanf_s(p, "%d", map[i][j], NULL);
+			fscanf_s(p, "%d,", &map[i][j]);
 		}
 	}
 	fclose(p);
@@ -27,17 +26,18 @@ float **tileEngine::loadMap()
 
 void tileEngine::dibujarMapa(float **map)
 {
-	int i, j;
-
+	int i=0, j=0;
+	float sy = 0;
+	float sx = 0;
 	
 
 	for (i = 0; i < columnas; i++) {
-		float sy = *map[i];
+		std::cout << map[i][j];
+		sx = map[i][j];
 		
 		for (j = 0; j < filas; j++) {
-			float sx = *map[j];
-			al_draw_bitmap_region(fondo, 0, 0, tilewidth, tileheight, sy, sx, NULL);
+			sy = map[i][j];
 		}
 	}
-
+	al_draw_bitmap_region(fondo, 0, 0, tilewidth, tileheight, sy, sx, NULL);
 }
--- TileEngine.h
+++ TileEngine.h
@@ -8,13 +8,14 @@ class tileEngine
 	private:
 		int tilewidth = 32;
 		int tileheight = 32;
-		int width, height;
+		int width = 32, height = 32;
 		int columnas = 40, filas = 22;
 		int mapSizeX = 40, mapSizeY = 22;
 		ALLEGRO_BITMAP* fondo = al_load_bitmap("IMG/Arena.bmp");
 		
 
 	public:
+		float getMap() { return **map; };
 		float **map;
 		float **loadMap();
 		void dibujarMapa(float **map);
